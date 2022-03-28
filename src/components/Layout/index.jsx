86f0af0e09d7ce5c97111a0de8fca15568d045fb
Jesús Monge <jesus_jaml@outlook.com>
2022-03-28 17:39:50 -0500

    Developed Layout component to wrapp all pages in the APP

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../Footer/Index'
 import Header from '../Header'
+import HomeMainContent from '../HomeMainContent'
 
 const Layout = () => {
   return (
     <div className="wrapper">
       <Header />
+      <HomeMainContent />
       <Footer />
     </div>
   )
