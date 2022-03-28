96d788b48fa427a124a7c9f7b9d28e34e4bc4ade
Jesús Monge <jesus_jaml@outlook.com>
2022-03-28 17:38:35 -0500

    Developed responsive Footer  component

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -8,24 +8,28 @@ const Footer = () => {
         alt="Dreadful Tomato"
         className="footer__logo"
       />
-      <ul>
-        <li>Home</li>
-        <li>Terms of Use</li>
-        <li>Legal Notices</li>
-        <li>Help</li>
-        <li>Mange Account</li>
-        <div className="app-container">
-          <img
-            src="https://i.postimg.cc/wMhdk4t5/app-store.png"
-            alt="app store"
-          />
-          <img
-            src="https://i.postimg.cc/SQ8qqRZX/google-play.png"
-            alt="google-play store"
-          />
-        </div>
-        <p>Copyright 2022 Tomato Streaming All Rights Reserved</p>
+      <ul className="footer-links">
+        <li className="footer-links__item">Home</li>
+        <li className="footer-links__item">Terms of Use</li>
+        <li className="footer-links__item">Legal Notices</li>
+        <li className="footer-links__item">Help</li>
+        <li className="footer-links__item">Mange Account</li>
       </ul>
+      <div className="footer-app">
+        <img
+          src="https://i.postimg.cc/wMhdk4t5/app-store.png"
+          alt="app store"
+          className="footer-app__img"
+        />
+        <img
+          src="https://i.postimg.cc/SQ8qqRZX/google-play.png"
+          alt="google-play store"
+          className="footer-app__img"
+        />
+      </div>
+      <p className="footer__copyright">
+        Copyright 2022 Tomato Streaming All Rights Reserved
+      </p>
     </footer>
   )
 }
