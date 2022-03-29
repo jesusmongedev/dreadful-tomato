8dea33ebf33e524bffedb642dd1cbf0828943c70
Jesús Monge <jesus_jaml@outlook.com>
2022-03-29 17:17:59 -0500

    Developed Movies and Series page

---
+++ src/components/MoviesPage/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MoviesPage = () => {
+  return (
+    <div>
+      MoviesPage
+      <Link to="/">Back to Home page</Link>
+    </div>
+  )
+}
+
+export default MoviesPage
