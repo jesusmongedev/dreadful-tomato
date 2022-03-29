b1aa2095abe97b236f15e33552b8cac8ef2c978c
Jesús Monge <jesus_jaml@outlook.com>
2022-03-29 16:36:43 -0500

    Fixed footer component view in large screens

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -3,33 +3,37 @@ import './styles.scss'
 const Footer = () => {
   return (
     <footer className="footer">
-      <img
-        src="https://i.postimg.cc/KjyNvQCM/logo.png"
-        alt="Dreadful Tomato"
-        className="footer__logo"
-      />
-      <ul className="footer-links">
-        <li className="footer-links__item">Home</li>
-        <li className="footer-links__item">Terms of Use</li>
-        <li className="footer-links__item">Legal Notices</li>
-        <li className="footer-links__item">Help</li>
-        <li className="footer-links__item">Mange Account</li>
-      </ul>
-      <div className="footer-app">
+      <div className="footer-top">
         <img
-          src="https://i.postimg.cc/wMhdk4t5/app-store.png"
-          alt="app store"
-          className="footer-app__img"
+          src="https://i.postimg.cc/KjyNvQCM/logo.png"
+          alt="Dreadful Tomato"
+          className="footer__logo"
         />
-        <img
-          src="https://i.postimg.cc/SQ8qqRZX/google-play.png"
-          alt="google-play store"
-          className="footer-app__img"
-        />
+        <ul className="footer-links">
+          <li className="footer-links__item">Home</li>
+          <li className="footer-links__item">Terms of Use</li>
+          <li className="footer-links__item">Legal Notices</li>
+          <li className="footer-links__item">Help</li>
+          <li className="footer-links__item">Mange Account</li>
+        </ul>
       </div>
-      <p className="footer__copyright">
-        Copyright 2022 Tomato Streaming All Rights Reserved
-      </p>
+      <div className="footer-bottom">
+        <div className="footer-app">
+          <img
+            src="https://i.postimg.cc/wMhdk4t5/app-store.png"
+            alt="app store"
+            className="footer-app__img"
+          />
+          <img
+            src="https://i.postimg.cc/SQ8qqRZX/google-play.png"
+            alt="google-play store"
+            className="footer-app__img"
+          />
+        </div>
+        <p className="footer__copyright">
+          Copyright 2022 Tomato Streaming All Rights Reserved
+        </p>
+      </div>
     </footer>
   )
 }
