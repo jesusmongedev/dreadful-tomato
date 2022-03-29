bad6ce0459062301b98a6defc0ce5cb912170765
Jesús Monge <jesus_jaml@outlook.com>
2022-03-29 17:17:35 -0500

    Added Link component to navigate to home page

--- src/components/HomeMainContent/HomeMovies/index.jsx
+++ src/components/HomeMainContent/HomeMovies/index.jsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const HomeMovies = () => {
   return (
-    <section className="section">
-      <img
-        src="https://i.postimg.cc/vBFJzdsD/movies.png"
-        alt="movies"
-        className="section__img section__img--filter"
-      />
-      <h2 className="section__tittle section__tittle--opacity">Movies</h2>
-    </section>
+    <Link to="/movies">
+      <section className="section">
+        <img
+          src="https://i.postimg.cc/vBFJzdsD/movies.png"
+          alt="movies"
+          className="section__img section__img--filter"
+        />
+        <h2 className="section__tittle section__tittle--opacity">Movies</h2>
+      </section>
+    </Link>
   )
 }
 
--- src/components/HomeMainContent/HomeSeries/index.jsx
+++ src/components/HomeMainContent/HomeSeries/index.jsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const HomeSeries = () => {
   return (
-    <section className="section">
-      <img
-        src="https://i.postimg.cc/hPf2f3kY/series.png"
-        alt="series"
-        className="section__img"
-      />
-      <h2 className="section__tittle">Series</h2>
-    </section>
+    <Link to="/series">
+      <section className="section">
+        <img
+          src="https://i.postimg.cc/hPf2f3kY/series.png"
+          alt="series"
+          className="section__img"
+        />
+        <h2 className="section__tittle">Series</h2>
+      </section>
+    </Link>
   )
 }
 
