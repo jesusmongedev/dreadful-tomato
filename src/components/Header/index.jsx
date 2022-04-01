ea4bc63ed55f7c667a9ac7b8133f09b101cd8579
Jesús Monge <jesus_jaml@outlook.com>
2022-03-31 20:01:15 -0500

    Builded dynamic Header depending on URL's pathname using react-router useLocation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,62 @@
+import { Link, useLocation } from 'react-router-dom'
 import './styles.scss'
 
 const Header = () => {
+  const { pathname } = useLocation()
+
   return (
-    <header className="header">
-      <img
-        src="https://i.postimg.cc/KjyNvQCM/logo.png"
-        alt="Dreadful Tomato"
-        className="header__logo"
-      />
+    <header className="header" id="top">
+      <Link to="/">
+        <img
+          src="https://i.postimg.cc/KjyNvQCM/logo.png"
+          alt="Dreadful Tomato"
+          className="header__logo"
+        />
+      </Link>
+
+      {pathname === '/movies' || pathname === '/series' ? (
+        <nav className="header-nav">
+          <Link to="/movies">
+            <div className="nav-container">
+              <img
+                src="https://i.postimg.cc/sMZkjywp/icon-movies.png"
+                alt="icon movies"
+                className="nav-container__icon"
+              />
+              <span>Movies</span>
+            </div>
+          </Link>
+          <Link to="/series">
+            <div className="nav-container">
+              <img
+                src="https://i.postimg.cc/6yTsPCZb/icon-series.png"
+                alt="icon movies"
+                className="nav-container__icon"
+              />
+              <span>Series</span>
+            </div>
+          </Link>
+          <div className="nav-container nav-container--end">
+            <img
+              src="https://i.postimg.cc/VdTyP5Bf/icon-filter.png"
+              alt="icon movies"
+              className="nav-container__icon"
+            />
+            <span>Filters</span>
+          </div>
+        </nav>
+      ) : (
+        ''
+      )}
+
       <div className="header-actions">
-        <a href="https://rviewer.io/" className="header-actions__user-login">
+        <Link to="/" className="header-actions__user-login">
           Login{' '}
           <img
             src="https://i.postimg.cc/yNf0pb2V/icon-login.png"
             alt="Login icon"
           />
-        </a>
+        </Link>
         <button className="primary-button">Start your free trial</button>
       </div>
     </header>
